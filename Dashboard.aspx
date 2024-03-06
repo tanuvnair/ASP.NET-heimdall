437a50f017ad49e124e14d0e5946900ccd39712b
Tanuv Nair <tanuvnair@gmail.com>
2024-03-06 19:25:34 +0530

    Minor changes

--- Dashboard.aspx
+++ Dashboard.aspx
@@ -1,7 +1,15 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Dashboard.aspx.cs" Inherits="ASP.NET_heimdall.Dashboard" MasterPageFile="~/Admin.Master" %>
 
 <asp:Content ContentPlaceHolderID="bodyContent" runat="server">
-    <form id="form1" runat="server">
-        <asp:SqlDataSource ID="SqlDataSource1" runat="server"></asp:SqlDataSource>
-    </form>
+    <%-- 
+        TO-DO List
+        - Handle sessions
+        - Add AttendanceRecords database table
+        - Add functionality to record attendance
+        - Add different views for different user roles (A normal user cannot view admin level stuff)
+        - Add table to perform CRUD operations on users
+        - Add a QR code generator
+    --%>
+
+
 </asp:Content>
--- Dashboard.aspx.designer.cs
+++ Dashboard.aspx.designer.cs
@@ -13,23 +13,5 @@ namespace ASP.NET_heimdall
 
     public partial class Dashboard
     {
-
-        /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-
-        /// <summary>
-        /// SqlDataSource1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
     }
 }
